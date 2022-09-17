996942e8488312155f88a1869f399845a43a63b7
Shantanu Jalkote <74247710+Shanatanu-J@users.noreply.github.com>
2022-09-17 18:19:10 +0530

    Complete Drum Kit Project Code

---
+++ index.js
@@ -0,0 +1,65 @@
+// FOr Mouse Click
+
+var numberOfButton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButton; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    // alert("I got clicked");
+    // console.log(this.innerHTML);
+    makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+  });
+}
+// FOr KeyPress
+document.addEventListener("keypress", function (event) {
+  //   console.log(event);
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+// Function for sound
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var kickBase = new Audio("sounds/kick-bass.mp3");
+      kickBase.play();
+      break;
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    default:
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
